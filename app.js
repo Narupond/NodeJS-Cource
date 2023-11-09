90aee3d25e9cbcf55d6cc0c564c6fa124a888639
Narupond <Naruporn.ph@mail.wu.ac.th>
2023-11-09 08:40:53 +0700

    update

--- app.js
+++ app.js
@@ -2,17 +2,22 @@ const express = require('express');
 const app = express();
 const debug = require('debug')('app');
 const morgan = require('morgan');
-const port = 5000;
+const PORT = process.env.PORT || 5000;
 const path = require('path');
 
 app.use(morgan('combined'));
 app.use(express.static(path.join(__dirname,"/public/")));
 
+app.set("views","./src/views");
+app.set("view engine", "ejs")
+
+app.get("/products")
+
 // ทำการจัดการกับ request ที่เข้ามา
 app.get('/', (req, res) => {
-    res.render('index', {username: 'Naruporn', customer: ["Nrp1", "Nrp2", "Nrp"]});
+    res.render('index',{username: 'Nrp', customer: ["A", "B", "C"]});
 })
 
-app.listen(port, () => {
-    debug(`Server is running on http://localhost:${port}`);
+app.listen(PORT, () => {
+    debug(`Server is running on http://localhost:${PORT}`);
   });
